16df6945cfc4df87dc38555abe3d586a93e74646
kabirshuvo <kabirshuvo19@gmail.com>
2023-05-08 01:13:02 +0600

    Getting form value in console

--- src/components/Updated.jsx
+++ src/components/Updated.jsx
@@ -5,6 +5,16 @@ const Updated = () => {
 
 const loadedUser = useLoaderData();
 
+const handleUpdate = event => {
+    event.preventDefault();
+
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+
+    console.log(name, email)
+}
+
 
     return (
         <div>
@@ -12,6 +22,15 @@ const loadedUser = useLoaderData();
 
             <Link to={`/users`}><button>User</button></Link>
             <h3>User Upgradation of {loadedUser.name} {loadedUser.email}</h3>
+
+        
+            <form onSubmit={handleUpdate}>
+                <input type="text" name="name" id="name" defaultValue={loadedUser.name} style={{marginBottom:'1rem'}}/>
+                <br  />
+                <input type="email" name="email" id="email" defaultValue={loadedUser.email} style={{marginBottom:'1rem'}}/>
+                <br />
+                <input style={{paddingRight:'1rem', paddingLeft:'1rem' }} type="submit" value="update" />
+            </form>
         </div>
     );
 };
