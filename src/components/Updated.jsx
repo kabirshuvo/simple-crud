dfc2a9a2204c849a9bdc4f3fca0ad2fa6ea6ccfd
kabirshuvo <kabirshuvo19@gmail.com>
2023-05-08 02:10:07 +0600

    User updated successfully :)))

--- src/components/Updated.jsx
+++ src/components/Updated.jsx
@@ -13,6 +13,22 @@ const handleUpdate = event => {
     const email = form.email.value;
 
     console.log(name, email)
+    const updatedUser = {name, email};
+
+    fetch(`http://localhost:5000/users/${loadedUser._id}`, {
+        method: 'PUT',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(updatedUser)
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        if(data.modifiedCount > 0){
+            alert(`user updated successfully`)
+        }
+    })
 }
 
 
